398e318e6bbb599f3147f99c5721aa44ca0b8776
sanghui <tkdgml719@gamil.com>
2024-03-11 13:49:48 +0900

    업데이트

--- 19_spring_mvc_practice2_ver2/src/main/java/com/application/practice2Ver2/service/MemberServiceImpl.java
+++ 19_spring_mvc_practice2_ver2/src/main/java/com/application/practice2Ver2/service/MemberServiceImpl.java
@@ -7,7 +7,6 @@ import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
-import org.apache.ibatis.javassist.expr.NewArray;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
